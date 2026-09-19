2d95b2db89ad1f01f0e1a6cdc689fe7041e68ae5
Riyadeshma <riyadeshma293@gmail.com>
2026-09-19 23:11:43 +0530

    done chnges

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('deposit/', views.deposit, name='deposit'),
+    path('withdraw/', views.withdraw, name='withdraw'),
+    path('transfer/', views.transfer, name='transfer'),
+    path('history/', views.history, name='history'),
     path('signup/', views.signup, name='signup'),
 ]
